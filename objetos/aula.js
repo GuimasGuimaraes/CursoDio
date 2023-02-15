8469e908d9142ce979e9aaa22f3418771f0637c9
GuimasGuimaraes <guilhermeguimaraes970@gmail.com>
2023-02-15 17:28:13 -0300

    novo commit

--- objetos/aula.js
+++ objetos/aula.js
@@ -3,4 +3,9 @@ const guilherme = {
   idade: 25,
   cor: "parda",
   altura: 1.73,
+  descrever: function () {
+    console.log(`Meu nome é ${this.nome} e minha idade é ${this.idade}`);
+  },
 };
+
+guilherme.descrever();
